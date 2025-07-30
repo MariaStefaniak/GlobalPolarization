0d4e770a174c72bbae56efb013d57cb5815ee4e4
Maria Stefaniak <43681217+MariaStefaniak@users.noreply.github.com>
2025-07-30 14:40:36 -0400

    Update ExecuteEPcalculations.cpp

--- ExecuteEPcalculations.cpp
+++ ExecuteEPcalculations.cpp
@@ -1,5 +1,5 @@
 void ExecuteEPcalculations(){
-    for(int ii = 1; ii <=1; ii++){
+    for(int ii = 1; ii <=3; ii++){
         gROOT->ProcessLine(".L calculateEventPlane.cpp+");
         gROOT->ProcessLine(Form("calculateEventPlane(%d)",ii));
     }
